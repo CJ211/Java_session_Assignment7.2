c7c1126f76a7ad2099d9d2add83893098ca3489b
CJ211 <shhahjay@gmail.com>
2017-01-10 22:41:23 +0530

    Create St.java

---
+++ St.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class St {
+
+   public static void main(String args[]) {
+      String Str = new String("Welcome to Tutorialspoint.com");
+
+      System.out.print("Return Value :" );
+      System.out.println(Str.substring(10) );
+
+   }
+}
